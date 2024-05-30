068fd51faa4845fd8ac51b04d3739fd8888da5fc
asilva7a <silva7a@cmich.edu>
2024-05-30 19:11:24 -0400

    Revised function to run more simply

--- Treatment_response.m
+++ Treatment_response.m
@@ -1,78 +1,131 @@
-function[data_table_delta, all_data] = treatment_response(all_data, cell_types, binSize, sortByVar)
-%% Goal: have the function pull spike times from  0-30min (1:54,000,000) and 30-60min (54,000,001:108,000,000) and 
-%% 60-90min (108,000,001:162,000,000) and calculate the firing rate for each cell in each time period. 
-%% Then, calculate the change in firing rate for each cell in response to treatment.
-% 
-% 
-% Plot and compare the change in firing rate of the cells in response to treatment. 
-% Ouptuts:
-% - A table of the change in firing rate of each Unit in response to treatment for statistical analysis
-% - A figure of the change in firing rate of each Unit in response to treatment
-%
-% Inputs:
-% - all_data: A structure containing all the data generated from SpikeTurnpike
-% - cell_types: A structure containing the cell types to plot (e.g. 'MSN', 'TAN')
-% - treatment_period: an numeric specifiying in minutes the time of treatment
- 
-groupNames = fieldnames(all_data);
-
-groupsVec = {};
-cellTypesVec = {};
-FRs_vec = [];
-
-% Loop through all the groups
-for groupNum = 1:length(groupNames)
-    % Code inside the for loop
-    group = groupNames{groupNum};
-    mouseNames = fieldnames(all_data.(groupName));
-    % Loop through all the mice
-    for MouseNum = 1:length(mouseNames)
-        mouseName = mouseNames{MouseNum};
-
-        cellIDs = fieldnames(all_data.(groupName).(mouseName)); % Loop through all the cells
-
-         for cellID_num = 1:length(cellIDs) % Loop through all the cells
-            cellID = cellIDs{cellID_num};
-
-            thisCellType = all_data.(groupName).(mouseName).(cellID).cellType; % Get the cell type of the cell
-            isSingleUnit = all_data.(groupName).(mouseName).(cellID).isSingleUnit; % Check if the cell is a single unit
-            if any(strcmp(cell_types, thisCellType)) && isSingleUnit % Check if the cell type is in the list
-                groupsVec{end+1,1} = groupName; % Add the group name to the groupsVec
-                cellTypesVec{end+1,1} = thisCellType; % Add the cell type to the cellTypesVec
-                recNames_vec{end+1,1} = mouseName; % Add the mouse name to the recNames_vec
-                cellIDs_vec{end+1,1} = cellID; % Add the cell ID to the cellIDs_vec
-            
-                if ~isempty(sortByVar)
-                    sortByVar_vec{end+1,1} = all_data.(groupName).(mouseName).(cellID).(sortByVar); % Add the sortByVar to the sortByVar_vec
-                end
-
-                if binSize == 0
-                    FRs_vec(end+1,1) = all_data.(groupName).(mouseName).(cellID).MeanFR_total;
-                else
-                    %calculate FR as max of 200s bins
-                    FRs_vec(end+1,1) = all_data.(groupName).(mouseName).(cellID).FR_binned;
-                    Spiketimes = all_data.(groupName).(mouseName).(cellID).SpikeTimes_all / all_data.(groupName).(mouseName).(cellID).Sampling_Frequency;
-                    RecDuration = all_data.(groupName).(mouseName).(cellID).RecordingDuration;
-                    intervalBounds = 0:binSize:RecDuration;
-                    binned_FRs_vec = [];
+function data_table_FR = FR_compare_Treatment(all_data, cell_types, binSize, plot_points, moment, period)
+    % Plot and compare ISI coefficient of variation (CV) between groups for periods before and after a specified moment.
+    %
+    % INPUTS
+    % - all_data
+    % - cell_types: cell array of cell types to plot. e.g., {'MSN','TAN'}
+    % - binSize: size of bins (seconds) to calculate FRs. The maximum of those
+    %            binned FRs will be the final saved FR of the unit. If 0, the
+    %            whole-recording FR is used instead.
+    % - plot_points: 0 to only plot bars (+/- SEM), 1 to plot points on top.
+    %       Useful to show the distribution.
+    % - moment: the specified moment (seconds) around which to analyze FRs.
+    % - period: the period (seconds) before and after the moment to consider for analysis.
+    
+    groupNames = fieldnames(all_data);
+    
+    groupsVec = {};
+    cellTypesVec = {};
+    FRs_vec = [];
+    timePeriodVec = {}; % To store time period information ('Before', 'After')
+    
+    for groupNum = 1:length(groupNames)
+        groupName = groupNames{groupNum};
+    
+        mouseNames = fieldnames(all_data.(groupName));
+    
+        for mouseNum = 1:length(mouseNames)
+            mouseName = mouseNames{mouseNum};
+    
+            cellIDs = fieldnames(all_data.(groupName).(mouseName));
+    
+            for cellID_num = 1:length(cellIDs)
+                cellID = cellIDs{cellID_num};
+    
+                thisCellType = all_data.(groupName).(mouseName).(cellID).Cell_Type;
+                isSingleUnit = all_data.(groupName).(mouseName).(cellID).IsSingleUnit;
+                if any(strcmp(cell_types, thisCellType)) && isSingleUnit
+                    % Calculate FR for the period before the moment
+                    spikeTimes = all_data.(groupName).(mouseName).(cellID).SpikeTimes_all / all_data.(groupName).(mouseName).(cellID).Sampling_Frequency;
+                    
+                    % Before the moment
+                    startTime = max(0, moment - period);
+                    endTime = moment;
+                    intervalBounds = startTime:binSize:endTime;
+                    binned_FRs_vec_before = [];
                     for ii = 1:length(intervalBounds)-1
-                        n_spikes = length(Spiketimes(Spiketimes > intervalBounds(ii) & Spiketimes <= intervalBounds(ii+1)));
-                        binned_FRs_vec(ii) = sum(Spiketimes > intervalBounds(ii) & Spiketimes <= intervalBounds(ii+1)) / binSize;
+                        n_spikes = length(spikeTimes((spikeTimes >= intervalBounds(ii))&(spikeTimes <= intervalBounds(ii+1))));
+                        binned_FRs_vec_before(end+1,1) = n_spikes / binSize;
                     end
-                    FRs_vec(end) = max(binned_FRs_vec);
+                    if ~isempty(binned_FRs_vec_before)
+                        FRs_vec(end+1,1) = max(binned_FRs_vec_before);
+                        groupsVec{end+1,1} = groupName;
+                        cellTypesVec{end+1,1} = thisCellType;
+                        timePeriodVec{end+1,1} = 'Before';
+                    end
+    
+                    % After the moment
+                    startTime = moment;
+                    endTime = min(all_data.(groupName).(mouseName).(cellID).Recording_Duration, moment + period);
+                    intervalBounds = startTime:binSize:endTime;
+                    binned_FRs_vec_after = [];
+                    for ii = 1:length(intervalBounds)-1
+                        n_spikes = length(spikeTimes((spikeTimes >= intervalBounds(ii))&(spikeTimes <= intervalBounds(ii+1))));
+                        binned_FRs_vec_after(end+1,1) = n_spikes / binSize;
+                    end
+                    if ~isempty(binned_FRs_vec_after)
+                        FRs_vec(end+1,1) = max(binned_FRs_vec_after);
+                        groupsVec{end+1,1} = groupName;
+                        cellTypesVec{end+1,1} = thisCellType;
+                        timePeriodVec{end+1,1} = 'After';
+                    end
                 end
             end
-        end 
+        end
     end
-end
-
-%% Plotting the data as violin plot for each group 
-figure;
-imagesc(data);  % This will create a heat map where each cell color represents a firing rate
-colorbar;  % Shows the color scale
-xlabel('Time (bins)');  % Label the x-axis
-ylabel('Neuron/Trial');  % Label the y-axis
-title('Firing Rate Over Time');
+    
+    %% Remove outliers (optional, commented out)
+    % FRs_vec_new = [];
+    % groupsVec_new = {};
+    % cellTypesVec_new = {};
+    % 
+    % for groupNum = 1:length(groupNames)
+    %     groupName = groupNames{groupNum};
+    %     for cell_type_ind = 1:length(cell_types)
+    %         cell_type_name = cell_types{cell_type_ind};
+    % 
+    %         FRs_vec_sub = FRs_vec(strcmp(groupsVec,groupName) & strcmp(cellTypesVec,cell_types{cell_type_ind}),1);
+    %         FRs_vec_sub = rmoutliers(FRs_vec_sub);
+    % 
+    %         groupsVec_sub = groupsVec(strcmp(groupsVec,groupName),1);
+    %         groupsVec_sub = groupsVec_sub(1:length(FRs_vec_sub),1);
+    % 
+    %         cellTypesVec_sub = cellTypesVec(strcmp(cellTypesVec,cell_type_name),1);
+    %         cellTypesVec_sub = cellTypesVec_sub(1:length(FRs_vec_sub),1);
+    % 
+    %         FRs_vec_new = [FRs_vec_new; FRs_vec_sub];
+    %         groupsVec_new = cat(1, groupsVec_new, groupsVec_sub);
+    %         cellTypesVec_new = cat(1, cellTypesVec_new, cellTypesVec_sub);
+    %     end
+    % end
+    % FRs_vec = FRs_vec_new;
+    % groupsVec = groupsVec_new;
+    % cellTypesVec = cellTypesVec_new;
+    
+    %% Plotting 
+    figure;
+    g = gramm('x',timePeriodVec, 'y',FRs_vec, 'color',groupsVec);
+    g.facet_grid([],cellTypesVec, "scale","independent");
+    g.stat_summary('type','sem', 'geom',{'bar','black_errorbar'}, 'width',0.6, 'dodge',0.8, 'setylim',true);
+    g.set_names('x','Time Period', 'y','Firing Rate (Hz)', 'Color','Group', 'Column','Cell Type');
+    g.no_legend;
+    g.draw();
+    
+    if plot_points
+        g.update('x',timePeriodVec, 'y',FRs_vec, "color",groupsVec);
+        g.geom_point('dodge',0.8);
+        g.set_color_options('lightness',40);
+        g.set_point_options("markers","^", "base_size",3);
+        g.no_legend;
+        g.draw;
+    end
+   
+    
+    %% Make data table to export for stats
+    data_table_FR = table(groupsVec, cellTypesVec, timePeriodVec, FRs_vec, 'VariableNames',{'Group','CellType','TimePeriod','FR'});
+    
+    end
+    
 
 
 
