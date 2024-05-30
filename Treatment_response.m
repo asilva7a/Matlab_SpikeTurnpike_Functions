71aa6eee6a61cc9a9e19824308c41796b731e5fd
asilva7a <silva7a@cmich.edu>
2024-05-30 18:10:02 -0400

    Quick description change

--- Treatment_response.m
+++ Treatment_response.m
@@ -1,4 +1,9 @@
-function[data_table_delta, all_data] = treatment_response(all_data, cell_types, binSize)
+function[data_table_delta, all_data] = treatment_response(all_data, cell_types, binSize, sortByVar)
+%% Goal: have the function pull spike times from  0-30min (1:54,000,000) and 30-60min (54,000,001:108,000,000) and 
+%% 60-90min (108,000,001:162,000,000) and calculate the firing rate for each cell in each time period. 
+%% Then, calculate the change in firing rate for each cell in response to treatment.
+% 
+% 
 % Plot and compare the change in firing rate of the cells in response to treatment. 
 % Ouptuts:
 % - A table of the change in firing rate of each Unit in response to treatment for statistical analysis
@@ -7,7 +12,7 @@ function[data_table_delta, all_data] = treatment_response(all_data, cell_types, 
 % Inputs:
 % - all_data: A structure containing all the data generated from SpikeTurnpike
 % - cell_types: A structure containing the cell types to plot (e.g. 'MSN', 'TAN')
-%
+% - treatment_period: an numeric specifiying in minutes the time of treatment
  
 groupNames = fieldnames(all_data);
 
@@ -26,8 +31,7 @@ for groupNum = 1:length(groupNames)
 
         cellIDs = fieldnames(all_data.(groupName).(mouseName)); % Loop through all the cells
 
-
-        for cellID_num = 1:length(cellIDs) % Loop through all the cells
+         for cellID_num = 1:length(cellIDs) % Loop through all the cells
             cellID = cellIDs{cellID_num};
 
             thisCellType = all_data.(groupName).(mouseName).(cellID).cellType; % Get the cell type of the cell
@@ -38,6 +42,9 @@ for groupNum = 1:length(groupNames)
                 recNames_vec{end+1,1} = mouseName; % Add the mouse name to the recNames_vec
                 cellIDs_vec{end+1,1} = cellID; % Add the cell ID to the cellIDs_vec
             
+                if ~isempty(sortByVar)
+                    sortByVar_vec{end+1,1} = all_data.(groupName).(mouseName).(cellID).(sortByVar); % Add the sortByVar to the sortByVar_vec
+                end
 
                 if binSize == 0
                     FRs_vec(end+1,1) = all_data.(groupName).(mouseName).(cellID).MeanFR_total;
@@ -59,13 +66,13 @@ for groupNum = 1:length(groupNames)
     end
 end
 
-%% Plotting the data as heat map over the recording period
+%% Plotting the data as violin plot for each group 
 figure;
-h = heatmap(groupsVec, cellTypesVec, FRs_vec, 'Colormap', jet, 'ColorLimits', [0 20]);
-h.Title = 'Firing rate of cells in response to treatment';
-h.XLabel = 'Groups';
-h.YLabel = 'Unit';
-h.ColorbarVisible = 'on';
+imagesc(data);  % This will create a heat map where each cell color represents a firing rate
+colorbar;  % Shows the color scale
+xlabel('Time (bins)');  % Label the x-axis
+ylabel('Neuron/Trial');  % Label the y-axis
+title('Firing Rate Over Time');
 
 
 
